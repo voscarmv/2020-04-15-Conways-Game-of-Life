0d39c87e00828304ec157f12cd4be0b02351c73d
Oscar Mier <voscarmv@gmail.com>
2020-04-15 13:04:50 -0500

    Copy world function.

--- main.js
+++ main.js
@@ -11,11 +11,19 @@ world[(Math.floor(h/2))][(Math.floor(w/2))] = "[0]";
 world[(Math.floor(h/2))-1][(Math.floor(w/2))-1] = "[0]";
 world[(Math.floor(h/2))+1][(Math.floor(w/2))+1] = "[0]";
 
+
+
 function tick(){
 //  document.getElementById("conway").innerHTML = "<h2>Hello world!</h2>"
 //  document.getElementById("conway").innerHTML = "h: " + world.length + ", h:" + world[0];  
   // alert(neighborhood((Math.floor(w/2)),(Math.floor(h/2))));
-  document.getElementById("conway").innerHTML = display(world);
+  let world2 = copyworld(world);
+  world2[(Math.floor(h/2))+1][(Math.floor(w/2))+2] = "[0]";
+  document.getElementById("conway").innerHTML = display(world2);
+}
+
+function copyworld(world){
+  return world.map(inner => inner.slice());
 }
 
 function display(world){
